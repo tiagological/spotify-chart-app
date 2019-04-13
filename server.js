2960b58b25cb9ed59f219a605e65dd7ee39b96e7
Tiago Marques <t.marques999@gmail.com>
2019-04-13 15:58:17 +0100

    Updated back end logic

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const axios = require('axios');
 const querystring = require('querystring');
 require('dotenv').config();
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
+
 const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
-
-let accessToken = '';
 
 app.get('/spotify-authorization', (req, res) => {
   axios({
@@ -32,6 +34,6 @@ app.get('/spotify-authorization', (req, res) => {
     });
 });
 
-app.get('/', (req, res) => res.send('Hello World from Express!'));
+// app.get('/', (req, res) => res.send('Hello World from Express!'));
 
 app.listen(port, () => console.log(`Express app listening on port ${port}!`));
