064c43882b80629147fe0a7d89718f33ba81ffe1
Tiago Marques <t.marques999@gmail.com>
2019-04-13 15:52:02 +0100

    Adding `build` to source control

---
+++ client/build/precache-manifest.667886df9c9646036d0b20318bedc9cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "19bf476b8ea56c70d4428b6a8c357e9c",
+    "url": "/static/media/music-concert.19bf476b.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ce1d1ff1eee4e1d06f6e",
+    "url": "/static/js/main.353b117f.chunk.js"
+  },
+  {
+    "revision": "f6b8da6999ff7aca4f04",
+    "url": "/static/js/2.1793a068.chunk.js"
+  },
+  {
+    "revision": "ce1d1ff1eee4e1d06f6e",
+    "url": "/static/css/main.7f95fdc1.chunk.css"
+  },
+  {
+    "revision": "2b6a79866a2a8156260125379976370c",
+    "url": "/index.html"
+  }
+];
