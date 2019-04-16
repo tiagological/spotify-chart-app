6de93536bfed29f24adb6caaa199cba363564015
Tiago Marques <t.marques999@gmail.com>
2019-04-16 15:14:23 +0100

    Updated build

---
+++ client/build/precache-manifest.d0a1318968704e88db88b3f9a81fd815.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "19bf476b8ea56c70d4428b6a8c357e9c",
+    "url": "/static/media/music-concert.19bf476b.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "53d563a8fc30ed47252a",
+    "url": "/static/js/main.5ed7edcc.chunk.js"
+  },
+  {
+    "revision": "f6b8da6999ff7aca4f04",
+    "url": "/static/js/2.1793a068.chunk.js"
+  },
+  {
+    "revision": "53d563a8fc30ed47252a",
+    "url": "/static/css/main.d9111f37.chunk.css"
+  },
+  {
+    "revision": "819108016be7d2a4037a951b19831254",
+    "url": "/index.html"
+  }
+];
